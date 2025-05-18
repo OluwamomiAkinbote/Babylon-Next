82df94155ced3576255db5ae5467a602312d92d0
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-05-18 15:19:13 +0100

    api url

--- src/components/config.js
+++ src/components/config.js
@@ -1,9 +1,8 @@
-// config.js
-const ENV = process.env.NEXT_PUBLIC_ENV || "production";
+// config.js (for Next.js)
+const ENV = process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV || "production";
 
-const API_URL =
-  ENV === "development"
-    ? process.env.NEXT_PUBLIC_API_LOCAL
-    : process.env.NEXT_PUBLIC_API_LIVE;
+const API_URL = ENV === "development" 
+  ? process.env.NEXT_PUBLIC_API_LOCAL || "http://localhost:8000"
+  : process.env.NEXT_PUBLIC_API_LIVE || "https://ayo.newstropy.online";
 
-export { ENV, API_URL };
+export { API_URL };
