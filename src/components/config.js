6b90b56adaedc83dfcacc792e09eaad32a754d20
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-05-18 15:44:03 +0100

    next config adjusted

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@
 const ENV = process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV || "production";
 
 const API_URL = ENV === "development" 
-  ? process.env.NEXT_PUBLIC_API_LOCAL || "http://localhost:8000"
+  ? process.env.NEXT_PUBLIC_API_LOCAL || "http://127.0.0.1:8000"
   : process.env.NEXT_PUBLIC_API_LIVE || "https://ayo.newstropy.online";
 
 export { API_URL };
