1bdf0d97ca7722c6980c4e35762de7c3f8e76ebb
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-05-18 21:45:16 +0100

    api adjustment

--- src/components/config.js
+++ src/components/config.js
@@ -2,10 +2,10 @@
 
 const ENV = process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV || "production";
 
-
+// Optional fallback to API_LIVE if API_LOCAL is not set in dev
 const API_URL =
   ENV === "development"
-    ? process.env.NEXT_PUBLIC_API_LOCAL 
-    : process.env.NEXT_PUBLIC_API_LIVE 
+    ? process.env.NEXT_PUBLIC_API_LOCAL || "http://127.0.0.1:8000"
+    : process.env.NEXT_PUBLIC_API_LIVE || "https://default-production-url.com";
 
 export { API_URL };
