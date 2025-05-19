86248ed7cfbcb5a95fe6d218e866dbc295aa49b5
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-05-19 10:12:41 +0100

    api exposed

--- src/components/config.js
+++ src/components/config.js
@@ -1,11 +1,11 @@
-// config.js (for Next.js)
 
-const ENV = process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV || "production";
 
-// Optional fallback to API_LIVE if API_LOCAL is not set in dev
+const ENV = "production"; 
+
+// API URLs
 const API_URL =
   ENV === "development"
-    ? process.env.NEXT_PUBLIC_API_LOCAL || "http://127.0.0.1:8000"
-    : process.env.NEXT_PUBLIC_API_LIVE || "https://default-production-url.com";
+    ? "http://127.0.0.1:8000"  
+    : "https://ayo.newstropy.online";  
 
 export { API_URL };
